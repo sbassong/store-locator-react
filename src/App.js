371adc40d7bcd65e568be1dd92406ac3b619858e
Sam Bassong <sam.bassong@gmail.com>
2022-04-13 13:05:55 -0700

    set up App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2Jhc3NvbmciLCJhIjoiY2wxdjc2NWJvMWQ3NzNrb2ZxbGlzaW1pbyJ9.RBURoPP0qGxffTP1e5zz4Q';
+
+
+
+export default function App() {
+  
+}
