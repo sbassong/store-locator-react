9784160fe46161d11e4b75c3eb9b974532c1e043
Sam Bassong <sam.bassong@gmail.com>
2022-04-13 13:17:19 -0700

    set up initial state and map

--- src/App.js
+++ src/App.js
@@ -6,5 +6,22 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2Jhc3NvbmciLCJhIjoiY2wxdjc2NWJvMWQ3NzNrb2Zxb
 
 
 export default function App() {
-  
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+
 }
